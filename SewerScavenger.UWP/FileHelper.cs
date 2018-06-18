425a30b42c6d40fcba2a4916b1564b03c6fd4b13
bnadala <40347812+bnadala@users.noreply.github.com>
2018-06-17 20:35:40 -0700

    Add files via upload

---
+++ SewerScavenger.UWP/FileHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using Xamarin.Forms;
+using SewerScavenger.UWP;
+using Windows.Storage;
+
+[assembly: Dependency(typeof(FileHelper))]
+namespace SewerScavenger.UWP
+{
+public class FileHelper : IFileHelper
+{
+    public string GetLocalFilePath(string filename)
+    {
+        return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+    }
+}
+}
